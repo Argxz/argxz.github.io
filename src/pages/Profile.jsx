2478cad8ad154394ef3ae35a438ce25508278bfd
Argxz <riyanargaw@gmail.com>
2024-11-12 17:04:13 +0700

    update profile pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from 'react';
 import '../styles/profile.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
