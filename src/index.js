88a642c57d81c4a6df55bf342a451ac1bc95bd0c
Argxz <riyanargaw@gmail.com>
2024-11-13 20:44:32 +0700

    define url base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+const myUrl = '/argxz.github.io'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename={`${myUrl}`}>
+      <App />
+    </Router>
   </React.StrictMode>
 );
